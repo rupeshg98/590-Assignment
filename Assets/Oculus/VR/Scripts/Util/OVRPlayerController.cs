dd45962cc9a6a72b897f65410ab7843627f545cc
shaikster101 <hshaik@live.unc.edu>
2020-02-23 19:00:39 -0500

    kaieja

--- Assets/Oculus/VR/Scripts/Util/OVRPlayerController.cs
+++ Assets/Oculus/VR/Scripts/Util/OVRPlayerController.cs
@@ -142,7 +142,7 @@ public class OVRPlayerController : MonoBehaviour
 	public float InitialYRotation { get; private set; }
 	private float MoveScaleMultiplier = 1.0f;
 	private float RotationScaleMultiplier = 1.0f;
-	private bool SkipMouseRotation = true; // It is rare to want to use mouse movement in VR, so ignore the mouse by default.
+	private bool SkipMouseRotation = false; // It is rare to want to use mouse movement in VR, so ignore the mouse by default.
 	private bool HaltUpdateMovement = false;
 	private bool prevHatLeft = false;
 	private bool prevHatRight = false;
--- Assets/TreasureHunter.cs
+++ Assets/TreasureHunter.cs
@@ -14,11 +14,13 @@ public class TreasureHunter : MonoBehaviour
 
     public TextMesh text3;
 
+    
+
     public Inventory myInventory; 
 
     List<GameObject> myList ; 
 
-    public Camera mainCam; 
+    //public Camera mainCam; 
 
     private int scoreCounter = 0; 
 
@@ -35,72 +37,93 @@ public class TreasureHunter : MonoBehaviour
     {
         
         
-        if(Input.GetMouseButtonDown(0)){
+        // if(Input.GetMouseButtonDown(0)){
 
 
 
             
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(mainCam.transform.position, mainCam.transform.forward, out hit))
-        {
-            if(hit.collider.gameObject.name.Contains("Sphere")){
+        // RaycastHit hit;
+        // // Does the ray intersect any objects excluding the player layer
+        // if (Physics.Raycast(mainCam.transform.position, mainCam.transform.forward, out hit))
+        // {
+        //     
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+       if(other.gameObject.name.Contains("Sphere")){
                 numItems++; 
 
                 text1.text = "Num Items: " + numItems; 
 
-                scoreCounter = scoreCounter + hit.collider.gameObject.GetComponent<Score>().points; 
+                scoreCounter = scoreCounter + other.gameObject.GetComponent<Score>().points; 
 
                 text2.text = "Score: " + scoreCounter;
 
-                Debug.Log(hit.collider.gameObject.GetComponent<Score>().prefabName);
+                Debug.Log(other.gameObject.GetComponent<Score>().prefabName);
 
-                myInventory.list.Add(Resources.Load(hit.collider.gameObject.GetComponent<Score>().prefabName, typeof(GameObject)) as GameObject);
+                myInventory.list.Add(Resources.Load(other.gameObject.GetComponent<Score>().prefabName, typeof(GameObject)) as GameObject);
 
-                text3.text = "I have Collected: " + hit.collider.gameObject.name;
+                text3.text = "I have Collected: " + other.gameObject.name;
 
-                Destroy(hit.collider.gameObject);
+                Destroy(other.gameObject);
 
             }
 
-            if(hit.collider.gameObject.name.Contains("Cube")){
+        if(other.gameObject.name.Contains("Cube")){
+            numItems++; 
+
+            text1.text = "Num Items: " + numItems; 
+
+            scoreCounter = scoreCounter + other.gameObject.GetComponent<Score>().points; 
+
+            text2.text = "Score: " + scoreCounter;
+
+            Debug.Log(other.gameObject.GetComponent<Score>().prefabName);
+
+            myInventory.list.Add(Resources.Load(other.gameObject.GetComponent<Score>().prefabName, typeof(GameObject)) as GameObject);
+
+            text3.text = "I have Collected: " + other.gameObject.name;
+
+            Destroy(other.gameObject);
+        }
+
+        if(other.gameObject.name.Contains("Capsule")){
                 numItems++; 
 
+            text1.text = "Num Items: " + numItems; 
+
+            scoreCounter = scoreCounter + other.gameObject.GetComponent<Score>().points; 
+
+            text2.text = "Score: " + scoreCounter;
+
+            Debug.Log(other.gameObject.GetComponent<Score>().prefabName);
+
+            myInventory.list.Add(Resources.Load(other.gameObject.GetComponent<Score>().prefabName, typeof(GameObject)) as GameObject);
+
+            text3.text = "I have Collected: " + other.gameObject.name;
+
+            Destroy(other.gameObject);
+        }
+        
+
+        if(other.gameObject.name.Contains("Cylinder")){
+                 numItems++; 
+
                 text1.text = "Num Items: " + numItems; 
 
-                scoreCounter = scoreCounter + hit.collider.gameObject.GetComponent<Score>().points; 
+                scoreCounter = scoreCounter + other.gameObject.GetComponent<Score>().points; 
 
                 text2.text = "Score: " + scoreCounter;
 
-                Debug.Log(hit.collider.gameObject.GetComponent<Score>().prefabName);
+                Debug.Log(other.gameObject.GetComponent<Score>().prefabName);
 
-                myInventory.list.Add(Resources.Load(hit.collider.gameObject.GetComponent<Score>().prefabName, typeof(GameObject)) as GameObject);
+                myInventory.list.Add(Resources.Load(other.gameObject.GetComponent<Score>().prefabName, typeof(GameObject)) as GameObject);
 
-                text3.text = "I have Collected: " + hit.collider.gameObject.name;
+                text3.text = "I have Collected: " + other.gameObject.name;
 
-                Destroy(hit.collider.gameObject);
+                Destroy(other.gameObject);
             }
-
-            if(hit.collider.gameObject.name.Contains("Capsule")){
- numItems++; 
-
-                text1.text = "Num Items: " + numItems; 
-
-                scoreCounter = scoreCounter + hit.collider.gameObject.GetComponent<Score>().points; 
-
-                text2.text = "Score: " + scoreCounter;
-
-                Debug.Log(hit.collider.gameObject.GetComponent<Score>().prefabName);
-
-                myInventory.list.Add(Resources.Load(hit.collider.gameObject.GetComponent<Score>().prefabName, typeof(GameObject)) as GameObject);
-
-                text3.text = "I have Collected: " + hit.collider.gameObject.name;
-
-                Destroy(hit.collider.gameObject);
-            }
-        }
-
-
-        }
     }
 }
